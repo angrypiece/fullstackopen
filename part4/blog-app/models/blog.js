f2bbc5729378ad4c5f0af94887a2f1224fab0480
angrypiece <273227391+angrypiece@users.noreply.github.com>
2026-04-26 18:03:24 +0300

    refactor: blog controller and model

---
+++ part4/blog-app/models/blog.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const config = require("../utils/config");
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl = config.MONGODB_URI;
+
+mongoose.connect(mongoUrl, { family: 4 });
+
+module.exports = Blog;
