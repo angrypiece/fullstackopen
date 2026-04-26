a09267a867917f81ff5be42fdb51e2fef8d67c3d
angrypiece <273227391+angrypiece@users.noreply.github.com>
2026-04-26 17:57:46 +0300

    refactor: config

--- part4/blog-app/index.js
+++ part4/blog-app/index.js
@@ -1,39 +1,38 @@
-const express = require('express')
-const mongoose = require('mongoose')
-require("dotenv").config()
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("./utils/config");
+const logger = require("./utils/logger");
 
-const app = express()
+const app = express();
 
 const blogSchema = mongoose.Schema({
   title: String,
   author: String,
   url: String,
   likes: Number,
-})
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = process.env.MONGO_URL
-mongoose.connect(mongoUrl, { family: 4 })
+const mongoUrl = config.MONGODB_URI;
+mongoose.connect(mongoUrl, { family: 4 });
 
+app.use(express.json());
 
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
+app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
+    response.json(blogs);
+  });
+});
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
 
   blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
+    response.status(201).json(result);
+  });
+});
 
-const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port: ${config.PORT}`);
+});
